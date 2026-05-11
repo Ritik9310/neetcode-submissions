9e4a02ebbd85b0a36a2ad93614ff4024fbf2ba4a
Ritik raj <ritikraj12070@gmail.com>
2026-05-12 00:53:56 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.cpp
@@ -0,0 +1,25 @@
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+      if(head == NULL && head -> next == NULL){
+        return NULL;
+      }
+        ListNode* curr = head;
+        ListNode*prev = NULL;
+        ListNode* forward = NULL;
+
+      while( curr != NULL ){
+
+          forward = curr -> next;
+          curr -> next = prev; 
+          prev = curr;
+          curr = forward;
+
+      }
+
+       return prev;
+    }
+};
